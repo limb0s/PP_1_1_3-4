691e0556fd16becb6603574234123f3e24101a16
Zaripov Rinat <eslimbos@gmail.com>
2024-09-06 18:17:17 +0300

    changes for PP_1_1_4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,18 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
-        userDao.createUsersTable();
-        userDao.saveUser("Egor", "Mironov", (byte) 20);
-        userDao.saveUser("Alex", "Gorsh", (byte) 22);
-        userDao.saveUser("Barbara", "Vening", (byte) 43);
-        userDao.saveUser("Ilya", "Belyaev", (byte) 54);
-        userDao.getAllUsers().forEach(System.out::println);
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+        userDaoHibernate.createUsersTable();
+        userDaoHibernate.saveUser("Egor", "Mironov", (byte) 20);
+        userDaoHibernate.saveUser("Alex", "Gorsh", (byte) 22);
+        userDaoHibernate.saveUser("Barbara", "Vening", (byte) 43);
+        userDaoHibernate.saveUser("Ilya", "Belyaev", (byte) 54);
+        userDaoHibernate.getAllUsers().forEach(System.out::println);
+        userDaoHibernate.cleanUsersTable();
+        userDaoHibernate.dropUsersTable();
+
+
+
+
 
 
     }
